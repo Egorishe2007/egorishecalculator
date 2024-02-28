827152d6a1a7b4d35d4840b9bfc4c294a65791ea
Egorishe2007 <67650023+Egorishe2007@users.noreply.github.com>
2024-02-28 19:59:02 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,14 +3,14 @@ const readlineSync = require('readline-sync');
 const calculator = {
   a: 0,
   b: 0,
-  read: function () {
+  read() {
     this.a = parseFloat(readlineSync.question('Enter the first number: '));
     this.b = parseFloat(readlineSync.question('Enter the second number: '));
   },
   sum() {
     return this.a + this.b;
   },
-  mul: function () {
+  mul() {
     return this.a * this.b;
   }
 };
